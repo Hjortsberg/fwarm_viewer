87d452c9b503902256ed325340cd8b5d5f34181d
Philip Hjortsberg <philip@hjortsberg.me>
2018-02-26 14:47:59 +0100

    rectangle bounding boxes around humans

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -337,9 +337,28 @@ private:
 	cv::HOGDescriptor hog;
 	hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
 	
-	vector<Rect> found, found_filtered;	
-	hog.detectMultiScale(depthColored, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+	std::vector<cv::Rect> found, found_filtered;	
+	hog.detectMultiScale(depthColored, found, 0, cv::Size(8,8), cv::Size(32,32), 1.05, 2);
 	
+	size_t i, j;
+        for (i=0; i<found.size(); i++)
+        {
+            cv::Rect r = found[i];
+            for (j=0; j<found.size(); j++)
+                if (j!=i && (r & found[j])==r)
+                    break;
+            if (j==found.size())
+                found_filtered.push_back(r);
+        }
+        for (i=0; i<found_filtered.size(); i++)
+        {
+	    cv::Rect r = found_filtered[i];
+            r.x += cvRound(r.width*0.1);
+	    r.width = cvRound(r.width*0.8);
+	    r.y += cvRound(r.height*0.06);
+	    r.height = cvRound(r.height*0.9);
+	    rectangle(color, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
+	}	
 }
   void saveImages(const cv::Mat &color, const cv::Mat &depth, const cv::Mat &depthColored)
   {
